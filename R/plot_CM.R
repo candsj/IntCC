ae8c274818ba2cfb7a53f53662755fd13e961fe4
candsj <candsj@hotmail.com>
2023-08-01 21:29:39 -0400

    Update plot function

--- R/plot_CM.R
+++ R/plot_CM.R
@@ -23,7 +23,18 @@ plot_CM <-
     if (is.null(result)) {stop("Result is not provided!")}
     CM <- result[[1]]
     clusterID <- result[[2]]
+    k=length(unique(clusterID))
     cluster_order=unlist(split(seq_along(clusterID), clusterID))
-    pheatmap::pheatmap(CM[cluster_order,cluster_order],treeheight_row = 0,treeheight_col = 0)
-    if (!is.null(data)) {pheatmap::pheatmap(data[cluster_order,],treeheight_row = 0,treeheight_col = 0)}
-  }
+    sample_annotation=data.frame(Sample=rep(paste0("Cluster ",1:k),lengths(split(seq_along(clusterID), clusterID))))
+    CM_order=CM[cluster_order,cluster_order]
+    rownames(CM_order)=rownames(sample_annotation)=cluster_order
+    pheatmap::pheatmap(CM_order,annotation_row = sample_annotation,cluster_rows=FALSE,cluster_cols=FALSE,
+                       show_rownames=FALSE,treeheight_row = 0,treeheight_col = 0)
+    if (!is.null(data)) {
+      data_order=data[cluster_order,]
+      rownames(data_order)=cluster_order
+      pheatmap::pheatmap(data_order,annotation_row = sample_annotation,cluster_rows=FALSE,cluster_cols=FALSE,
+                         show_rownames=FALSE,treeheight_row = 0,treeheight_col = 0)
+      }
+    }
+
